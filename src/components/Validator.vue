9ef5a94bc24a0e2bd5a3bd4ebb3f5e7a02a26bab
Ryan Morris <ryansmorris@hotmail.com>
2022-04-15 12:32:14 +1000

    UI update

--- src/components/Validator.vue
+++ src/components/Validator.vue
@@ -1,25 +1,26 @@
 <template>
-    <div class="d-flex justify-content-center">
-        <div class="card w-40 p-4">
-            <h1 class="py-2">Enter a Tax File Number below</h1>
-            <h4 class="text-muted py-2">Don't worry, we don't store anything</h4>
-            <p class="py-2">
-            An Australian Tax File Number is not just a random sequence of numbers.<br>
-            It can be validated using a check digit. Read more
-            <a href="https://en.wikipedia.org/wiki/Tax_file_number#Check_digit" target="_blank" rel="noopener">here</a>.
-            </p>
-            
-            <div class="mb-3">
-                <input v-model="tfn" placeholder="123456789">
-            </div>
+    <div class="col-xs-12">
+        <div class="d-flex justify-content-center">
+            <div class="card p-4">
+                <h1 class="py-2">Enter a Tax File Number below</h1>
+                <h4 class="text-muted py-2">Don't worry, we don't store anything</h4>
+                <p class="py-2">
+                An Australian Tax File Number is not just a random sequence of numbers.<br>
+                It can be validated using a check digit. Read more
+                <a href="https://en.wikipedia.org/wiki/Tax_file_number#Check_digit" target="_blank" rel="noopener">here</a>.
+                </p>
+                
+                <div class="mb-3">
+                    <input v-model="tfn" placeholder="123456789">
+                </div>
 
-            <div>
-                <p v-if="(tfn.length == 8 || tfn.length == 9) && isValid == true" class="text-success">That is a valid Tax File Number!</p>
-                <p v-else-if="(tfn.length == 8 || tfn.length == 9) && isValid == false" class="text-danger">That is not a valid Tax File Number.</p>
-                <p v-else-if="tfn.length > 0 && tfn.length < 8" class="text-warning">TFN is not long enough</p>
-                <p v-else-if="tfn.length > 9" class="text-warning">TFN is too long</p>
+                <div>
+                    <p v-if="(tfn.length == 8 || tfn.length == 9) && isValid == true" class="text-success">That is a valid Tax File Number!</p>
+                    <p v-else-if="(tfn.length == 8 || tfn.length == 9) && isValid == false" class="text-danger">That is not a valid Tax File Number.</p>
+                    <p v-else-if="tfn.length > 0 && tfn.length < 8" class="text-warning">TFN is not long enough</p>
+                    <p v-else-if="tfn.length > 9" class="text-warning">TFN is too long</p>
+                </div>
             </div>
-            
         </div>
     </div>
 </template>
